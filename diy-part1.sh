009e7ccd534f03ec245710d5f653ec8c5c9a3484
zhangsean <zxf2342@qq.com>
2021-01-18 12:02:38 +0800

    Add tty

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+sed -i '$a src-git openwrt https://github.com/project-openwrt/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
